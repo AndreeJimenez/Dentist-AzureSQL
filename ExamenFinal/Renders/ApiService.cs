6e39a8e95bc6ea3aabced5cedc64ffbe3b5a4528
knatb <knatb@noreply.github.com>
2020-06-22 17:19:32 -0500

    Adding get dates from patient

--- ExamenFinal/Renders/ApiService.cs
+++ ExamenFinal/Renders/ApiService.cs
@@ -91,6 +91,44 @@ namespace ExamenFinal.Services
             }
         }
 
+        public async Task<ApiResponse> GetListDataAsyncByID<T>(string controller, int id)
+        {
+            try
+            {
+                var client = new HttpClient
+                {
+                    BaseAddress = new System.Uri(ApiUrl)
+                };
+                var response = await client.GetAsync(controller + "/" + id);
+                var result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new ApiResponse
+                    {
+                        IsSuccess = false,
+                        Message = result
+                    };
+                }
+                var data = JsonConvert.DeserializeObject<ObservableCollection<T>>(result);
+                return new ApiResponse
+                {
+                    IsSuccess = true,
+                    Message = "Ok",
+                    Result = data
+                };
+            }
+            catch (System.Exception ex)
+            {
+                return new ApiResponse
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+                    Result = null
+                };
+            }
+        }
+
         public async Task<ApiResponse> PostDataAsync(string controller, object data)
         {
             try
--- ExamenFinal/ViewModels/PatientDateViewModel.cs
+++ ExamenFinal/ViewModels/PatientDateViewModel.cs
@@ -1,10 +1,95 @@
-﻿using System;
+﻿using ExamenFinal.Models;
+using ExamenFinal.Services;
+using ExamenFinal.Views;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 
 namespace ExamenFinal.ViewModels
 {
-    class PatientDateViewModel
+    public class PatientDateViewModel : BaseViewModel
     {
+        Command _DateSelectCommand;
+        public Command DateSelectCommand => _DateSelectCommand ?? (_DateSelectCommand = new Command(DateSelectAction));
+
+        int _IDPatient;
+        public int IDPatient
+        {
+            get => _IDPatient;
+            set => SetProperty(ref _IDPatient, value);
+        }
+
+        int _IDDate;
+        public int IDDate
+        {
+            get => _IDDate;
+            set => SetProperty(ref _IDDate, value);
+        }
+
+        ObservableCollection<DateConsult> _DateConsult;
+        public ObservableCollection<DateConsult> DateConsult
+        {
+            get => _DateConsult;
+            set => SetProperty(ref _DateConsult, value);
+        }
+
+        List<DateConsult> _DateConsultonPatient;
+        public List<DateConsult> DateConsultonPatient
+        {
+            get => _DateConsultonPatient;
+            set => SetProperty(ref _DateConsultonPatient, value);
+        }
+
+        DateConsult _DateSelected;
+        public DateConsult DateSelected
+        {
+            get => _DateSelected;
+            set => SetProperty(ref _DateSelected, value);
+        }
+
+        public PatientDateViewModel() { }
+
+        public PatientDateViewModel(DatePatient dp)
+        {
+            if (dp != null)
+            {
+                IDDate = dp.IdDate;
+                IDPatient = dp.IdPatient;
+            }
+        }
+
+        public async void GetListOrderProducts()
+        {
+            try
+            {
+                ApiResponse response = await new ApiService().GetListDataAsyncByID<DateConsult>("DatePatient", IDDate);
+                if (response != null || response.Result != null)
+                {
+                    DateConsult = (ObservableCollection<DateConsult>)response.Result;
+                    if (response.Result != null)
+                    {
+                        DateConsultonPatient = DateConsult.ToList();
+                    }
+                    else
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Citas del Paciente", "No cuenta con ninguna cita", "Ok");
+                        await Application.Current.MainPage.Navigation.PopAsync();
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+        private void DateSelectAction()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new DateDetailPage(DateSelected));
+        }
     }
 }
